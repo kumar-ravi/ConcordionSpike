04ea4d8bff211ef603f7b9c25460bfa700433f91
kumar-ravi <kumarr@thoughtworks.com>
2013-05-23 08:53:50 +0530

    [Ravi]: Added steps to search for a flight

--- src/com/example/FlightSearch.java
+++ src/com/example/FlightSearch.java
@@ -1,5 +1,14 @@
 package com.example;
 
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import java.util.List;
+
 public class FlightSearch {
 
     public String flightNumber(String flightNumber) {
@@ -8,9 +17,24 @@ public class FlightSearch {
 
     }
 
-    public String flightOrigin(String flightOrigin) {
+    public String SearchForFlights(String flightOrigin, String flightDestination) {
+
+        WebDriver driver = new FirefoxDriver();
+        driver.get("http://goindigo.in");
+
+        Select originAirport = new Select(driver.findElement(By.id("from1Select")));
+
+        originAirport.selectByVisibleText(flightOrigin);
+
+        Select destinationAirport = new Select(driver.findElement(By.id("to1Select")));
+
+        destinationAirport.selectByVisibleText(flightDestination);
+
+        WebElement flightSearchButton = driver.findElement(By.xpath("//div[@class='buttonAlign']/a/img"));
+        flightSearchButton.click();
+
         String orig = String.format(flightOrigin);
-        System.out.println(orig);
+
         return String.format("Flight Origin Airport: " + flightOrigin);
 
 

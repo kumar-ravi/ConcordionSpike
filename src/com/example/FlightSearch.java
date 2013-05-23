f84af0ca5961185fc96e9a2ddb095ac050849dd2
kumar-ravi <kumarr@thoughtworks.com>
2013-05-23 13:04:46 +0530

    [Ravi]-Added Selenium jar file and modified the test for handling alerts

--- src/com/example/FlightSearch.java
+++ src/com/example/FlightSearch.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.Alert;
 
 import java.util.List;
 
@@ -22,16 +23,29 @@ public class FlightSearch {
         WebDriver driver = new FirefoxDriver();
         driver.get("http://goindigo.in");
 
+        WebElement oneWay = driver.findElement(By.id("oneWayRadio"));
+        oneWay.click();
+
+        Alert oneWayAlert = driver.switchTo().alert();
+        System.out.print(oneWayAlert.getText());
+        oneWayAlert.accept();
+
         Select originAirport = new Select(driver.findElement(By.id("from1Select")));
 
         originAirport.selectByVisibleText(flightOrigin);
 
         Select destinationAirport = new Select(driver.findElement(By.id("to1Select")));
 
+
         destinationAirport.selectByVisibleText(flightDestination);
+
 
         WebElement flightSearchButton = driver.findElement(By.xpath("//div[@class='buttonAlign']/a/img"));
         flightSearchButton.click();
+
+        Alert sameDayFlightAlert = driver.switchTo().alert();
+        System.out.print(sameDayFlightAlert.getText());
+        sameDayFlightAlert.accept();
 
         String orig = String.format(flightOrigin);
 
